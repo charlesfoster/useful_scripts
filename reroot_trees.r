862055937641bd4ae756011802c190e3315dbfb4
charlesfoster <charles.foster@sydney.edu.au>
2017-03-07 12:45:29 +1100

    Add files via upload

---
+++ reroot_trees.r
@@ -0,0 +1,20 @@
+# REROOT MULTIPLE TREES
+
+outgroup = commandArgs(trailingOnly=TRUE)
+
+library(ape)
+
+print("Your outgroup is: ")
+outgroup
+
+trees <- grep("tre", dir(), value = T)
+
+gene_tree <- vector()
+
+	for(i in 1:length(trees)){
+	gene_tree <- read.nexus(trees[i])
+	print(paste('I am working on this tree:', trees[i]))
+	treeRooted <- root(gene_tree, outgroup, resolve.root=TRUE)
+	output_name <- gsub(' ', '_', paste('rooted', trees[i]))
+	write.nexus(treeRooted, file = output_name)
+}
