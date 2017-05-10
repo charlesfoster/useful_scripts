c1d9cac4213902d3091227220c3962c717c3dd67
charlesfoster <charles.foster@sydney.edu.au>
2017-05-10 10:34:16 +1000

    Update raxml_script.py

--- raxml_script.py
+++ raxml_script.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python2.7
+# This is a script to generate the correct command for an analysis with RAxML
+# It saves you remembering what a lot of the common commands do
+# Additionally, you can choose the option to run the analysis immediately after creating the input
+# Usage: ./raxml_script.py
+
 import os
 
 test_version = raw_input("I'm going to assume you're using 'raxmlHPC-PTHREADS-AVX'. Is this correct? Y or N?\n> ")
@@ -17,11 +23,11 @@ threads = raw_input("How many threads do you wanna use?\n")
 print "I am going to envoke the rapid bootstrapping algorithm, if you don't like this, you can write your own script.\n*****WARNING: this will probably only work with the latest version(s) of RAxML.*****\nThe options, seeds and model are:\n"
 first_option = "-f a -x 19828 -p 1291 -m GTRGAMMA"
 print first_option
-
+print "Note: you can change these seeds afterwards if you want.\n"
 test_outgroup = raw_input("\nDo you want to use an outgroup? Y or N?\n> ")
 
 if test_outgroup == "Y" or test_outgroup == "y":
-    outgroup = raw_input("What is your outgroup? Enter the names of the taxa followed by a comma. If you don't have one, leave this blank)\n> ")
+    outgroup = raw_input("What is your outgroup? Enter the names of the taxa followed by a comma. If you don't have one and still (strangely) chose this option, leave this blank)\n> ")
 
 elif test_outgroup == "N" or test_outgroup == "n":
     outgroup = ""
@@ -42,8 +48,8 @@ script_name = raw_input("What should we call the script?\n> ")
 
 print "OK, so your analysis will be run like so:\n"
 
-print "./%s -T %s %s -o %s -q %s -s %s -n %s -N %s -k" % (version, threads, first_option, outgroup, partitions, alignment, analysis_name, boots)
 full_analysis = "./%s -T %s %s -o %s -q %s -s %s -n %s -N %s -k" % (version, threads, first_option, outgroup, partitions, alignment, analysis_name, boots)
+print full_analysis
 
 print "\nAnd your script will be called: %s" % script_name
 
@@ -53,6 +59,7 @@ if checkpoint == "Y" or checkpoint == "y":
     open(script_name,'w').writelines(full_analysis) 
     print "OK, I've written your command to file."
     os.system("say I love you") 
+
 elif checkpoint == "N" or checkpoint == "n": 
     print "Well, you must have made a mistake. Start again! \a\a\a\a\a\a\a"
 
