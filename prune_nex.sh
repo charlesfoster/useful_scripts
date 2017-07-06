95241341cf292ac564bc99ea99fe98173c9d2fee
charlesfoster <charles.foster@sydney.edu.au>
2017-07-06 14:36:01 +1000

    Update prune_nex.sh

--- prune_nex.sh
+++ prune_nex.sh
@@ -3,11 +3,11 @@
 ### Usage: ./prune_nex.sh taxa_to_keep.txt
 for i in *.nex;
 do
-sed '/matrix/q' $i >> ${i%.nex}_final.nex
-while read j; do grep "^$j" $i >> ${i%.nex}_final.nex;done < keep.txt;
-echo ";" >> ${i%.nex}_final.nex
-echo "end;" >> ${i%.nex}_final.nex
+sed '/matrix/q' $i >> ${i%.nex}_pruned.nex
+while read j; do grep "^$j" $i >> ${i%.nex}_pruned.nex;done < keep.txt;
+echo ";" >> ${i%.nex}_pruned.nex
+echo "end;" >> ${i%.nex}_pruned.nex
 KEEP_LEN=`wc -l keep.txt | sed "s|^ *||g"| sed "s| keep.txt||g"`
 NTAX_ORIG=`grep -o "ntax=[0-9]*" $i`
-sed -i '' "s|${NTAX_ORIG}|ntax=${KEEP_LEN}|g" ${i%.nex}_final.nex
+sed -i '' "s|${NTAX_ORIG}|ntax=${KEEP_LEN}|g" ${i%.nex}_pruned.nex
 done
